bd1e145cd56bc1fae9336611356c216ff779ee98
liangchen <765837431@qq.com>
2020-05-31 20:03:20 +0800

    aoligai

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("info")
     public String hello(){
-        return "hello，lclclc";
+        return "hello，lclclc aoligai";
     }
 }
