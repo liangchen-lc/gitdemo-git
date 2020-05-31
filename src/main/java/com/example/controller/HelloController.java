baaa7562b82c99fbfc336d03eae5f7f57908cbc9
liangchen <765837431@qq.com>
2020-05-31 18:57:02 +0800

    管理员第一次提交真实项目

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("info")
+    public String hello(){
+        return "hello，lclclc";
+    }
+}
